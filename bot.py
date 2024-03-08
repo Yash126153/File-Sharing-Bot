63f55eb85de45cb6ed0c017226dec999abdf8746
Yash126153 <162720723+Yash126153@users.noreply.github.com>
2024-03-08 21:30:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,7 +81,3 @@ class Bot(Client):
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
-
-    async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.")
